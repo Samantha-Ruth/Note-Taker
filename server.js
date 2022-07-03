62c4190e414a5c80d6d11e8ed962c296963c3c2f
Samantha-Ruth <sam.haberman@gmail.com>
2022-07-03 13:39:25 -0600

    separated out create new note function from api call

--- server.js
+++ server.js
@@ -43,55 +43,77 @@ app.get("/api/notes", (req, res) => {
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
-    res.json(results);
-  });
+  res.json(results);
+});
 
 function findByID(text_id, notesArray) {
   const result = notesArray.filter(note => note.text_id === text_id)[0];
   return result;
-  }; 
+};
 
 app.get("/api/notes/:id", (req, res) => {
-    const result = findByID(req.params.id, notes);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
+  const result = findByID(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
+}
+
+
+function createNewNote(body, notesArray) {
+  // destructuring assignment for items in req.body
+  const { title, text } = body;
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      text_id: randomUUID()
+    };
+    // Read the file, then re-write the file to put inside an array.
+    fs.readFile(`./Develop/db/db.json`, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+      } else {
+        const parsedNote = JSON.parse(data)
+        parsedNote.push(newNote)
+        fs.writeFile(`./Develop/db/db.json`, JSON.stringify(parsedNote), (err) => err ? console.error(err) : console.log('Note entered and ID created.'))
+      }
+    });
+    console.log(`${newNote.title} note has been written to JSON file`);
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    console.log(response);
+    return(response);
+  }
+};
+
 
 app.post("/api/notes", (req, res) => {
   // log that a POST request was received
   console.info(`${req.method} request was received to add a note`);
-  // destructuring assignment for items in req.body
-  const { title, text } = req.body;
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        text_id: randomUUID()
-        };
-  // Read the file, then re-write the file to put inside an array.
-      fs.readFile(`./Develop/db/db.json`, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err)
-        }else {
-          const parsedNote = JSON.parse(data)
-          parsedNote.push(newNote)
-          fs.writeFile(`./Develop/db/db.json`, JSON.stringify(parsedNote), (err)=> err ? console.error(err) : console.log('Note entered and ID created.'))
-        }
-      });
-        console.log(`${newNote.title} note has been written to JSON file`);
-        const response = {
-          status: 'success',
-          body: newNote,
-          };
-        console.log(response);
-        res.json(response);
-        }
-      });
+  // Maybe validation is within the IF TITLE AND TEXT
+  if (!validateNote(req.body)) {
+    res.status(400).send("This note is not properly formatted.")
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
 
 // ****  APP DELETE HERE *** 
 // app.delete("/api/notes", (req, res) => {
@@ -101,42 +123,42 @@ app.post("/api/notes", (req, res) => {
 app.delete('/notes/:text_id', (req, res, next) => {
   const id = req.params.id;
   Note.delete(id, (err) => {
-   if (err) return next(err);
-   res.send({ message: 'Deleted' });
+    if (err) return next(err);
+    res.send({ message: 'Deleted' });
   });
- });
+});
 
-  // log that a POST request was received
-  // console.info(`${req.method} request was received to delete a note`);
-  // // destructuring assignment for items in req.body
-  // const { title, text, text_id } = req.body;
-  //   // If all the required properties are present
-  //   if (title || text || text-id) {
-  //     // Variable for the object we will save
-  //     const newNote = {
-  //       title,
-  //       text,
-  //       text_id: randomUUID()
-  //       };
-  // // Read the file, then re-write the file to put inside an array.
-  //     fs.readFile(`./Develop/db/db.json`, 'utf8', (err, data) => {
-  //       if (err) {
-  //         console.error(err)
-  //       }else {
-  //         const parsedNote = JSON.parse(data)
-  //         parsedNote.push(newNote)
-  //         fs.writeFile(`./Develop/db/db.json`, JSON.stringify(parsedNote), (err)=> err ? console.error(err) : console.log('Note entered and ID created.'))
-  //       }
-  //     });
-  //       console.log(`${newNote.title} has been deleted.`);
-  //       const response = {
-  //         status: 'deleted',
-  //         body: newNote,
-  //         };
-  //       console.log(response);
-  //       res.json(response);
-  //       }
-  //     });
+// log that a POST request was received
+// console.info(`${req.method} request was received to delete a note`);
+// // destructuring assignment for items in req.body
+// const { title, text, text_id } = req.body;
+//   // If all the required properties are present
+//   if (title || text || text-id) {
+//     // Variable for the object we will save
+//     const newNote = {
+//       title,
+//       text,
+//       text_id: randomUUID()
+//       };
+// // Read the file, then re-write the file to put inside an array.
+//     fs.readFile(`./Develop/db/db.json`, 'utf8', (err, data) => {
+//       if (err) {
+//         console.error(err)
+//       }else {
+//         const parsedNote = JSON.parse(data)
+//         parsedNote.push(newNote)
+//         fs.writeFile(`./Develop/db/db.json`, JSON.stringify(parsedNote), (err)=> err ? console.error(err) : console.log('Note entered and ID created.'))
+//       }
+//     });
+//       console.log(`${newNote.title} has been deleted.`);
+//       const response = {
+//         status: 'deleted',
+//         body: newNote,
+//         };
+//       console.log(response);
+//       res.json(response);
+//       }
+//     });
 
 //   app.delete("/api/notes", (req, res) => {
 //     // let results = notes;
@@ -157,7 +179,7 @@ app.get('/', (req, res) => {
 //**** End of Catch Code ****/
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
 
 
